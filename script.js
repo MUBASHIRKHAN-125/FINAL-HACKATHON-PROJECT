23af1e93e26395732a84df39f50280c6d2848dc2
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-01-24 18:38:59 +0500

    responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('#element', {
+    strings: ['Healthcare and lot more,', 'Daily Meals,' , 'Monthly Family Support,' , 'IT and vocational trainigs' , 'Education' , 'Clean water'],
+    typeSpeed: 70,
+    backSpeed:60,
+    loop:true,
+    infinite:true
+  });
