1e991d6b05e455ba745d7d41c9b147b5a78db88a
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-01-28 15:59:04 +0500

    Edit.

---
+++ home.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('.multiple-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow: false,
+      nextArrow: false,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      dots: true,
+      focusOnSelect: true,
+      centerMode: true,
+      responsive: [
+        {
+          breakpoint: 995,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 580,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false,
+          },
+        },
+      ],
+    });
+  });
